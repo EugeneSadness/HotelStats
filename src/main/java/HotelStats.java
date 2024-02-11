83966696523c41c3e3ac1f681ab8c0cafce50c8d
Eugene Damm <eugenedamm2003@gmail.com>
2024-02-11 19:00:21 +0600

    HotelStats

---
+++ src/main/java/HotelStats.java
@@ -0,0 +1,34 @@
+import com.opencsv.CSVParser;
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.List;
+
+public class HotelStats {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String filePath = "C:\\Users\\Eugene\\Downloads\\archive\\hotels.csv";
+        CSVReader reader = new CSVReader(new FileReader(filePath));
+        List<String[]> allRows = reader.readAll();
+        int numOfRows = allRows.size();
+        System.out.println("Number of rows: " + numOfRows);
+        Thread.sleep(2000);
+
+        HashMap<String, Integer> hotelMap = new HashMap<>(); // Contains [CityName:NumberOfHotels]
+
+        int numOfThreads = 1;
+        ReadThread[] threads = new ReadThread[numOfThreads];
+        for (int i = 0; i < numOfThreads; i++) {
+            threads[i] = new ReadThread(allRows, (numOfRows/numOfThreads)*i, (numOfRows/numOfThreads)*(i+1), hotelMap);
+            threads[i].t.start();
+        }
+
+        for (int i = 0; i < numOfThreads; i++) {
+            threads[i].t.join();
+        }
+
+        System.out.println(hotelMap.keySet().isEmpty());
+    }
+}
---
+++ src/main/java/ReadThread.java
@@ -0,0 +1,42 @@
+import com.opencsv.CSVReader;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+
+public class ReadThread implements Runnable {
+    Thread t;
+    List<String[]> allRows;
+    HashMap<String, Integer> hotelMap;
+    int start;
+    int end;
+
+
+    public ReadThread(List<String[]> allRows, int start, int end, HashMap hotelMap) {
+        t = new Thread(this);
+        this.start = start;
+        this.end = end;
+        this.allRows = allRows;
+        this.hotelMap = hotelMap;
+    }
+
+    @Override
+    public void run() {
+        int col = 3;
+        for(int i = start; i < end; i++){
+            try {
+                String data = allRows.get(i)[col];
+                System.out.println("№" + i + ": " + data);
+                synchronized (hotelMap) {
+                    if (!hotelMap.containsKey(data)) {
+                        hotelMap.put(data, 1);
+                    } else
+                        hotelMap.put(data, hotelMap.get(data) + 1);
+
+                }
+            } catch (ArrayIndexOutOfBoundsException e){
+                continue;
+            }
+        }
+    }
+}
