7b9fc47d2c84b630c39d8efbc6ad4cd8802c4a36
Eugene Damm <eugenedamm2003@gmail.com>
2024-02-12 23:21:59 +0600

    Second commit

--- src/main/java/HotelStats.java
+++ src/main/java/HotelStats.java
@@ -1,15 +1,30 @@
-import com.opencsv.CSVParser;
 import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HotelStats {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         String filePath = "C:\\Users\\Eugene\\Downloads\\archive\\hotels.csv";
-        CSVReader reader = new CSVReaderBuilder(new FileReader(filePath));
-        System.out.println("Number of rows: " + reader.readNext()[3]);
+        CSVReader reader = new CSVReader(new FileReader(filePath));
+        List<String[]> allRows = reader.readAll();
+        int size = allRows.size();
+        int numOfThreads = 50;
+        HashMap<String , Integer> hotelMap = new HashMap<>();
+        ReadThread[] threads = new ReadThread[numOfThreads];
+        for (int i = 0; i < numOfThreads; i++) {
+            threads[i] = new ReadThread(filePath, (size/numOfThreads)*i, (size/numOfThreads)*(i+1), hotelMap);
+            threads[i].t.start();
+        }
+
+        for (int i = 0; i < numOfThreads; i++) {
+            threads[i].t.join();
+        }
+
+        System.out.println(hotelMap.keySet());
     }
 }
 
